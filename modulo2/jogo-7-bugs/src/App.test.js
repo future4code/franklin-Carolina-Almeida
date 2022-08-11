55b9128f978d2d2679c95c1136881b3dd69c2d90
carolinabratos <carolinaboalmeida@gmail.com>
2022-08-11 00:04:12 -0300

    encontrei os sete erros

--- modulo2/jogo-7-bugs/src/App.test.js
+++ modulo2/jogo-7-bugs/src/App.test.js
@@ -22,3 +22,11 @@ test("Teste 2", () => {
     })
   );
 });
+
+  // 1- calcula o valor errado quando seleciono todos os itens
+  // 2- não filtra quando o valor máximo é 2
+  // 3- mostra 5 produtos quando o valor mínimo é 2
+  // 4- digito qualquer coisa no campo e-mail e ele aceita
+  // 5- quando seleciono "conjunto fogo grátis". o valor final da compra é zero, apesar de o valor do item ser 250,00
+  // 6- o total não calcula corretamente ao adicionar e excluir itens
+  // 7- não tem a opção "limpar carrinho" conforme informado nos requisitos

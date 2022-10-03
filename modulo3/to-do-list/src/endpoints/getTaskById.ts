0ae1f0065dacd9ca1cab86f05dbfc3e91fa2081d
carolinabratos <carolinaboalmeida@gmail.com>
2022-10-03 17:37:09 -0300

    finalizando to do list

---
+++ modulo3/to-do-list/src/endpoints/getTaskById.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import moment = require("moment");
+import selectTaskById from "../data/selectTaskById";
+
+export default async function getTaskById (
+    req:Request,
+    res:Response
+){
+    try {
+        const result = await selectTaskById(req.params.id)
+
+        if(!result){
+            res.status(400).send({
+                message:"Tarefa não enontrada"
+            })
+            return
+        }
+
+        res.status(200).send({
+            id: result.id,
+            title:result.title,
+            description: result.description,
+            deadline: moment(result.deadline, 'YYYY-MM-DD').format('DD-MM-YYYY'),
+            status: result.status,
+            authorId: result.author_id,
+            authorNickname: result.nickname
+
+        })
+    } catch (error) {
+        res.status(400).send({
+            message: error.message || error.sqlMessage
+        })
+    }
+}

09f62b5c90253014ed1bf34121661724cbd6b369
carolinabratos <carolinaboalmeida@gmail.com>
2022-10-12 15:28:37 -0300

    criptografia-e-user-roles

---
+++ modulo3/criptografia-e-user-roles/src/services/HashMenage.ts
@@ -0,0 +1,15 @@
+import { compare, genSalt, hash } from "bcryptjs"
+
+export class HashMenage {
+    public hash = async (password: string) => {
+        const rounds = Number("12")
+        const salt = await genSalt(rounds)
+        const result = await hash(password, salt)
+        return result
+
+    }
+    public compare = async (password: string, hash: string) => {
+        const result = await compare(password, hash)
+        return result
+    }
+}
